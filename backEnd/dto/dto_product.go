59da737f2162663bfb79b759b0706f812875d526
GonzaGomezPizarro <1912988@ucc.edu.ar>
2022-06-03 11:21:04 -0300

    .

--- backEnd/dto/dto_product.go
+++ backEnd/dto/dto_product.go
@@ -1,10 +1,11 @@
 package dto
 
 type ProductDto struct {
-	ProductId int     `json:"product_id"`
-	Name      string  `json:"name"`
-	Picture   string  `json:"picture_url"`
-	Price     float32 `json:"product_unit_price"`
+	ProductId   int     `json:"product_id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Picture     string  `json:"picture_url"`
+	Price       float32 `json:"product_unit_price"`
 }
 
 type ProductsDto []ProductDto
