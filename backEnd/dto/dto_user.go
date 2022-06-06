d8c4dabf420abd8a4d15bdeaaffc8075ac030663
Belen Aguilar <63293979+belenaguilarv@users.noreply.github.com>
2022-06-06 17:03:07 -0300

    Update dto_user.go

--- backEnd/dto/dto_user.go
+++ backEnd/dto/dto_user.go
@@ -1,17 +1,12 @@
 package dto
 
 type UserDto struct {
-<<<<<<< HEAD
-	//UserId   int    `json:"id"`
-=======
->>>>>>> 973f972940c6aa743ddeacd2ce2a382ca1df3a56
 	Id       int    `json:"id"`
 	Name     string `json:"user_name"`
 	Password string `json:"password"`
-	//Token    string `json:"token"` // para encryptar password
-	//City     string `json:"city"`
-	//	Street   string `json:"street"`
-	//	Number   int    `json:"number"`
+	City     string `json:"city"`
+	Street   string `json:"street"`
+	Number   int    `json:"number"`
 }
 
 type UsersDto []UserDto
