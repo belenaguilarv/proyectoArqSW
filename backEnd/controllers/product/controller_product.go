7524ec0fcbe4cf26d96e0eed17a89db519c217b4
belenaguilarv <belenaguiilarv@gmail.com>
2022-06-12 14:41:16 -0300

    falta funcion byCategory

--- backEnd/controllers/product/controller_product.go
+++ backEnd/controllers/product/controller_product.go
@@ -2,16 +2,41 @@ package controllers
 
 import (
 	"net/http"
+	"strconv"
 
+	"github.com/belenaguilarv/proyectoArqSW/backEnd/dto"
+	service "github.com/belenaguilarv/proyectoArqSW/backEnd/services"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 )
 
-func GetProducts(c *gin.Context) {
+var jwtKey = []byte("secret_key")
 
+func GetProducts(c *gin.Context) {
+	var productsDto dto.ProductsDto
+	productsDto, err := service.ProductService.GetProducts()
+
+	if err != nil {
+		c.JSON(err.Status(), err)
+		return
+	}
+
+	c.JSON(http.StatusOK, productsDto)
 }
 func GetProductById(c *gin.Context) {
 	log.Debug("Product id to load: " + c.Param("id"))
+
+	id, _ := strconv.Atoi(c.Param("id"))
+	var productDto dto.ProductDto
+
+	productDto, err := service.ProductService.GetProductById(id)
+
+	if err != nil {
+		c.JSON(err.Status(), err)
+		log.Error(err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, productDto)
 }
 func GetProductByCategory(c *gin.Context) {
 	c.JSON(http.StatusOK, "")
