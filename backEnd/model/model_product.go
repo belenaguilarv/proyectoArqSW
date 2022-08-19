e2b4bd465bee383ceefc1584afb6e4ac79c271ae
GonzaGomezPizarro <1912988@ucc.edu.ar>
2022-08-19 15:45:16 -0300

    .

--- backEnd/model/model_product.go
+++ backEnd/model/model_product.go
@@ -1,13 +1,13 @@
 package model
 
 type Product struct {
-	Id   int    `gorm:"primaryKey; int; not null; unsigned; auto_increment"`
-	Name string `gorm:"type:varchar(350); not null; unique"`
-	//Description string  `gorm:"type:varchar(350); not null"`
-	//Picture     string  `gorm:"type:varchar(350); not null"`
-	Price      float32 `gorm:"type:float; not null"`
-	CategoryId int     `gorm:"type:int; unsigned; not null"`
-	Stock      int     `gorm:"type:smallint; unsigned; not null"`
+	Id          int     `gorm:"primaryKey; int; not null; unsigned; auto_increment"`
+	Name        string  `gorm:"type:varchar(350); not null; unique"`
+	Description string  `gorm:"type:varchar(350); not null"`
+	Picture     string  `gorm:"type:varchar(350); not null"`
+	Price       float32 `gorm:"type:float; not null"`
+	CategoryId  int     `gorm:"type:int; unsigned; not null"`
+	Stock       int     `gorm:"type:smallint; unsigned; not null"`
 }
 
 type Products []Product
