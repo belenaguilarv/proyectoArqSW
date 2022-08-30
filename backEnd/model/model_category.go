674c10a820d3c5bf8c60696ada639d29e1e0b966
GonzaGomezPizarro <1912988@ucc.edu.ar>
2022-08-30 14:22:42 -0300

    detalles

--- backEnd/model/model_category.go
+++ backEnd/model/model_category.go
@@ -1,5 +1,6 @@
 package model
 
 type Category struct {
-	Id int `gorm:"primaryKey; int; not null; unsigned; auto_increment"`
+	Id   int    `gorm:"primaryKey; int; not null; unsigned; auto_increment"`
+	name string `gorm:"type:varchar(255); not null"`
 }
