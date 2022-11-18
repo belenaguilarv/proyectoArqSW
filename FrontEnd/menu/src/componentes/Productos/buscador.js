ef9c8f0946b6811ababa9585a7967bc67acb5605
belenaguilarv <belenaguiilarv@gmail.com>
2022-11-18 11:34:53 -0300

    css

--- FrontEnd/menu/src/componentes/Productos/buscador.js
+++ FrontEnd/menu/src/componentes/Productos/buscador.js
@@ -60,6 +60,7 @@ export const ProductosBuscador = ()=>{
         onClick = {handleSubmit}
         />
       </div>
+        <div className="vacio">
         <div className="productos">
             {
                 productos.map(producto =>(
@@ -74,6 +75,7 @@ export const ProductosBuscador = ()=>{
                   /> 
                 ))
             }
+        </div>
         </div>
         </>
     )
